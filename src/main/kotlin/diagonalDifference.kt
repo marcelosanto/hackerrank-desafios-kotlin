350cdbf69b77969c6c847c709cac7e5c141b63ed
Marcelo Santos <rocha.mer21@gmail.com>
2022-04-10 01:01:30 -0300

    resolvido o exercicio - diagonalDifference

--- src/main/kotlin/diagonalDifference.kt
+++ src/main/kotlin/diagonalDifference.kt
@@ -6,27 +6,22 @@
  */
 
 fun diagonalDifference(arr: Array<Array<Int>>): Int {
-    // Write your code here
-//    val sumX = arr[0][0] + arr[1][1] + arr[2][2]
-//    val sumY = arr[0][2] + arr[1][1] + arr[2][0]
-//
-//    return if (sumX > sumY) sumX - sumY else sumY - sumX
 
     val n = arr.size
     var sumX = 0
     var sumY = 0
 
-    for(i in 0 until n){
-        for(j in 0 until n){
-        // an element from the main diagonal
-        if(i == j) {
-            sumX += arr[i][j];
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+
+            if (i == j) {
+                sumX += arr[i][j];
+            }
+
+            if (i + j == n - 1) {
+                sumY += arr[i][j];
+            }
         }
-        // an element from the counterdiagonal
-        if(i + j == n - 1){
-            sumY += arr[i][j];
-        }
-    }
     }
     return if (sumX > sumY) sumX - sumY else sumY - sumX
 }
@@ -37,7 +32,7 @@ fun main(args: Array<String>) {
     val arr = Array<Array<Int>>(n) { Array<Int>(n) { 0 } }
 
     for (i in 0 until n) {
-        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+        arr[i] = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
     }
 
     val result = diagonalDifference(arr)
